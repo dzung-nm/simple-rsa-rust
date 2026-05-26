630865223f0d22dcb423c4e5ae5a7c1adfe25302
Dzung Nguyen Mau <nguyenmaudung@gmail.com>
2026-05-26 15:11:23 +0700

    Update RSA examples

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod encryption_exponent;
 mod generate_prime_pair;
 mod miller_rabin;
 mod pkcs1_padding;
+
 pub mod rsa;
 
 // Suppress panic output for all tests
@@ -10,4 +11,3 @@ pub mod rsa;
 fn init_test() {
     std::panic::set_hook(Box::new(|_| {}));
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-use simple_rsa_rust::rsa::*;
+use simple_rsa_rust::rsa::{decrypt, encrypt, new_keys};
 
 fn main() {
-    let bits = 512;
+    let bits = 1024;
     let (public_key, private_key) = new_keys(bits);
     let messages = vec![
         b"Hello, RSA!".to_vec(),
-        b"Rust is great for cryptography.".to_vec(),
-        b"Simple RSA implementation.".to_vec(),
+        b"4E636AF98E40F3ADCFCCB698F4E80B9F".to_vec(), // A random session key for AES-128
+        b"A9993E364706816ABA3E25717850C26C9CD0D89D".to_vec(), // AN SHA-1 hash of a string
     ];
     for message in messages {
+        println!("--------------------------------------------------");
         let encrypted = encrypt(&message, &public_key);
         let decrypted = decrypt(&encrypted, &private_key);
-        assert_eq!(message.to_vec(), decrypted);
+        assert_eq!(decrypted, message);
         println!("Original message: {:?}", String::from_utf8_lossy(&message));
-        println!("Encrypted message: {}", encrypted);
-        println!(
-            "Decrypted message: {:?}",
-            String::from_utf8_lossy(&decrypted)
-        );
+        println!("Encrypted message in hex: {}", format!("{:X}", encrypted));
     }
 }
