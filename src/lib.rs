5b4c2eb8244d38fcd596505409c0e100cd6b6729
Dzung Nguyen Mau <nguyenmaudung@gmail.com>
2026-05-26 11:11:41 +0700

    Add dev-dependencies for testing: include `ctor` and suppress panic output

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,11 @@ mod generate_prime_pair;
 mod miller_rabin;
 mod pkcs1_padding;
 pub mod rsa;
+
+// Suppress panic output for all tests
+#[cfg(test)]
+#[ctor::ctor(unsafe)]
+fn init_test() {
+    std::panic::set_hook(Box::new(|_| {}));
+}
+
